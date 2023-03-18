06f5d4acb64c3dfce8d429e2e1de2efbb3755bbf
Mohammed Masud <masudm6@coventry.ac.uk>
2023-03-18 19:02:03 +0000

    add paid user section to user route permissions

--- permissions/users.js
+++ permissions/users.js
@@ -21,7 +21,17 @@ function defineAbilitiesFor(user) {
     can("delete", "User", { _id: user._id });
   }
 
-  // paid users ...
+  // paid users
+  if (user.role.name === "user") {
+    //console.log('standard user role')
+
+    can("read", "User", { _id: user._id }); // can read own details except for password/passwordSalt
+    cannot("read", "AllUsers"); // cannot read other user's details
+    cannot("read", "UserPassword");
+
+    can("update", "User", { _id: user._id }); // only for the fields: ‘firstName’, ‘lastName’, ‘about’, ‘password’, ‘email’, ‘avatarURL’
+    can("delete", "User", { _id: user._id });
+  }
 
   // admin users
   if (user.role.name === "admin") {
