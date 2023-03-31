b0de94cfe974e1bec51423462ced33bee3c937b4
Mohammed Masud <masudm6@coventry.ac.uk>
2023-03-31 18:26:23 +0000

    add random postcode search, search lat long via query params and postcode via request body param

--- routes/search.js
+++ routes/search.js
@@ -14,23 +14,24 @@ const Search = require("../models/Search");
 const createAbilityFor = require("../permissions/search");
 
 // helpers
-const { getPostcode, validatePostcode } = require("../helpers/postcode");
+const { getPostcode, getRandomPostcode, validatePostcode } = require("../helpers/postcode");
 const {
     getRelatedStops,
     getRelatedCrimes,
     linkAtco,
 } = require("../helpers/search");
 
-router.post("/", auth, bodyParser(), searchArea); // search for details of a lat and long area
-router.get("/:postcode", auth, searchPostcode); // searches by lat and long internally
+router.post("/", auth, bodyParser(), searchArea); // POST - finds a postcode internally
+router.get("/", auth, bodyParser(), searchPostcode); // GET - verifies and searches using postcode from the request body
 router.get("/random", auth, searchRandom); // admins only - for testing
 
 async function searchArea(cnx) {
+    // POST and latitude/longitude in query params
     // allows anyone to search via a lat and long in the body of the request
     // returns a list of property listings, transport nodes and crime.
 
     // no validation on the lat and long for now
-    const { latitude, longitude } = cnx.request.body;
+    const { latitude, longitude } = cnx.query;
 
     const lat = latitude;
     const long = longitude;
@@ -50,6 +51,8 @@ async function searchArea(cnx) {
 
     if (ability.can("read", "Search")) {
         // reverse lookup lat long to postcode to generate data for postcode field
+        // await FindPostcode with lat long
+        // await getPostcode with postcode
 
         cnx.body = `lat: ${lat} long: ${long}`;
     } else {
@@ -59,19 +62,15 @@ async function searchArea(cnx) {
 }
 
 async function searchPostcode(cnx) {
-    // allows anyone to search via a postcode - dangerous as they can currently enter anything here.
-    // returns a list of property listings, transport nodes and crime.
+    // GET and postcode in body.
+    // allows anyone to search via a postcode in the request body.
+    // returns a list of property listings, transport nodes and crime data
 
-    let { postcode } = cnx.params;
+    let { postcode } = cnx.request.body;
 
     if (!postcode) {
         cnx.status = 400;
         cnx.body = "Please provide a postcode.";
-        return;
-    } else if (postcode === "random") {
-        // temporary workaround
-        // in the future, postcode should be from the body instead of a path param
-        await searchRandom(cnx);
         return;
     }
 
@@ -123,6 +122,7 @@ async function searchPostcode(cnx) {
             cnx.status = 200;
             cnx.body = body;
         } else {
+            // invalid postcode
             cnx.status = 400;
             cnx.body = "Please provide a valid postcode.";
         }
@@ -133,7 +133,57 @@ async function searchPostcode(cnx) {
 }
 
 async function searchRandom(cnx) {
-    //admins only
-    cnx.status = 404;
+
+    let { user } = cnx.state;
+    if (!user) {
+        user = User({ role: Role({ name: "none" }) });
+    }
+    const ability = createAbilityFor(user);
+
+    if (ability.can("read", "RandomSearch")) {
+            const processedPostcode = await getRandomPostcode();
+            const dbPostcode = await Postcode.findOne({
+                postcode: processedPostcode.postcode,
+            });
+
+            // check for existing search by comparing latitude - Extremely unlikely as random postcodes are generated
+            const existingSearch = await Search.findOne({
+                latitude: dbPostcode.latitude,
+            });
+
+            if (!existingSearch) {
+                console.log("Saving new Random Search");
+                // save the search to the database
+                const newSearch = new Search({
+                    Postcode: dbPostcode,
+                    reverseLookup: true,
+                    latitude: dbPostcode.latitude,
+                    longitude: dbPostcode.longitude,
+                    Northing: dbPostcode.northings,
+                    Easting: dbPostcode.eastings,
+                });
+
+                await newSearch.save();
+                await linkAtco(newSearch);
+                await getRelatedStops(newSearch); // get all bus stops for location and link to search model
+                await getRelatedCrimes(newSearch); // get all crimes for location and link to search model
+            } else {
+                console.log(
+                    "Existing search found, ID:",
+                    existingSearch.searchID
+                );
+            }
+
+            const SearchModel = await Search.findOne({
+                latitude: dbPostcode.latitude,
+            }).populate(["Postcode", "queryBusStops"]);
+            
+            const body = SearchModel;
+            cnx.status = 200;
+            cnx.body = body;
+    } else {
+        cnx.status = 403;
+        cnx.body = "You are not authorised to view this resource";
+    }
 }
 module.exports = router;
