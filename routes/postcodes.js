62514db4178309d5d6efb7a92e6f2cd6d671aeb8
Mohammed Fardhin Masud <masudm6@coventry.ac.uk>
2023-04-12 01:04:18 +0000

    added docstrings to postcode route

--- routes/postcodes.js
+++ routes/postcodes.js
@@ -23,6 +23,18 @@ router.get("/", auth, getAllPostcodes); // allows admins to view all the postcod
 router.get("/random", auth, getRandomPostcodeRoute); // only authenticated users can access this route
 router.get("/:postcode", auth, getPostcodeRoute); // Search for specific valid postcode.
 
+/**
+ * Allows admins (or anyone who can "readAll" "Postcode" objects) to view all the postcodes that have been saved in the `Postcodes` collection.
+ * 
+ * @async
+ * @function getAllPostcodes
+ * 
+ * @param {Object} cnx - Koa context object
+ * @returns {undefined} Nothing, updates the context object with the postcodes from the database.
+ * 
+ * @see getAllPostcodes - fetches all the postcodes from the database.
+ * 
+ */
 async function getAllPostcodes(cnx) {
   const { user } = cnx.state;
   if (!cnx.state.user) {
@@ -42,6 +54,18 @@ async function getAllPostcodes(cnx) {
   }
 }
 
+/**
+ * Returns a random postcode from the database. Only authenticated users can access this route.
+ * 
+ * @async
+ * @function getRandomPostcodeRoute
+ * 
+ * @param {Object} cnx - Koa context object
+ * @returns {undefined} Nothing, updates the context's response body with the random postcode returned from getRandomPostcode.
+ * 
+ * @see getRandomPostcode - fetches a random postcode using the postcode.io API.
+ * 
+ */
 async function getRandomPostcodeRoute(cnx) {
   const { user } = cnx.state;
   if (!cnx.state.user) {
@@ -63,6 +87,19 @@ async function getRandomPostcodeRoute(cnx) {
   }
 }
 
+/**
+ * Returns a postcode from the database. Only authenticated users can access this route.
+ * 
+ * @async
+ * @function getPostcodeRoute
+ * 
+ * @param {Object} cnx - Koa context object
+ * @returns {undefined} Nothing, updates the response body with the postcode returned from getPostcode.
+ * 
+ * @see getPostcode - fetches a postcode using the postcode.io API, or a cached version from the database.
+ * @see validatePostcode - validates the postcode using the postcode.io API.
+ * 
+ */
 async function getPostcodeRoute(cnx) {
   let { postcode } = cnx.params;
 
