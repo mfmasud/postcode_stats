07d8909d1b503122c1e92211a371c7816acb859c
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-08-05 18:43:23 +0000

    removed old index.js to allow for a compiled version

--- index.js
+++ index.js
@@ -1,55 +1,13 @@
-require("dotenv").config();
-
-const logger = require("./utils/logger");
-
-// logger.info(process.env)
-
-const Koa = require("koa");
-const app = new Koa();
-
-const database = require("./helpers/database");
-
-const special = require("./routes/special");
-const postcodes = require("./routes/postcodes");
-const users = require("./routes/users");
-const search = require("./routes/search");
-
-app.use(special.routes());
-app.use(postcodes.routes());
-app.use(users.routes());
-app.use(search.routes());
-
-let port = process.env.PORT;
-
-/**
- * Initializes the databases and starts the Koa server.
- *
- * @async
- * @function startServer
- *
- * @param {Object} app - The Koa application object.
- * @param {Number} port - The port to listen on for incoming requests.
- *
- * @returns {Promise} A Promise that resolves when the server is started/listening.
- * @throws {Error} Throws an error if the server fails to start.
- *
- */
-async function startServer(app, port) {
-  try {
-    // reset dummy dbs in order
-    await database.connectDB(true);
-    await database.initUserDB();
-    await database.resetDataDB();
-    await database.initLocationDB();
-
-    // start koa server
-    app.listen(port, () => {
-      logger.info(`Server running on port ${port}`);
-    });
-  } catch (error) {
-    logger.error(`Error starting server:\n${error.message}`);
-  }
-}
-(async () => {
-  await startServer(app, port);
-})();
+import fastify from 'fastify';
+const server = fastify();
+server.get('/ping', async (request, reply) => {
+    return 'pong\n';
+});
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
+//# sourceMappingURL=index.js.map
