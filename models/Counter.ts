bdd33e62522067e76ed52cd475eb84886fa45d25
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-08-09 21:36:08 +0000

    added an increment function to the counter schema and made it easier to use from other schemas

--- models/Counter.ts
+++ models/Counter.ts
@@ -1,15 +1,29 @@
-import { Schema, model } from 'mongoose';
+// see https://mongoosejs.com/docs/typescript.html for how to use TypeScript with Mongoose
 
-interface ICounter {
-    counterName: string;
-    seq: number;
-}
+import { Schema, model, type InferSchemaType, type HydratedDocument, type Model } from 'mongoose';
 
-const counterSchema = new Schema({
+const counterSchema = new Schema(
+  {
     counterName: { type: String, required: true, unique: true },
     seq: { type: Number, default: 0 },
-  });
+  },
+);
 
-const Counter = model<ICounter>('Counter', counterSchema);
 
-export default Counter;
+counterSchema.statics.next = async function next(counterName: string): Promise<number> {
+  const doc = await this.findOneAndUpdate(
+    { counterName },
+    { $inc: { seq: 1 } },
+    { new: true, upsert: true }
+  ).lean();
+  return doc!.seq;
+};
+
+export interface CounterModel extends Model<Counter> {
+    next(counterName: string): Promise<number>;
+  }
+
+export type Counter = InferSchemaType<typeof counterSchema>;
+export type CounterDoc = HydratedDocument<Counter>;
+
+export default model<Counter, CounterModel>('Counter', counterSchema);
