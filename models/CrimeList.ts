acbc4cc4e474398dd25e803ad99d83455ba70df0
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-08-11 10:40:25 +0000

    added comments to crimelist ID generation

--- models/CrimeList.ts
+++ models/CrimeList.ts
@@ -64,14 +64,14 @@ crimeListSchema.pre('save', async function (this: CrimeListDoc) {
   // if the document is not new, do not set a new ID
   if (!this.isNew) return;
 
-  let newID = await Counter.next('crimeList');
+  let newID = await Counter.next('crimeList'); // get the next ID
 
   const CrimeListModel = model<CrimeList>('CrimeList');
-  while (await CrimeListModel.exists({ crimeListID: newID })) {
-    newID = await Counter.next('crimeList'); // if this 
+  while (await CrimeListModel.exists({ crimeListID: newID })) { // if the ID already exists,
+    newID = await Counter.next('crimeList'); // increment the ID and try again
   }
 
-  this.crimeListID = newID;
+  this.crimeListID = newID; // set the new ID
 });
 
 
