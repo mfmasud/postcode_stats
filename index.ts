fcdf23a8161a3084866dd198a9995548e9893f9e
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-08-20 20:12:26 +0000

    add graceful shutdown code from blog

--- index.ts
+++ index.ts
@@ -26,7 +26,16 @@ async function startServer( port: number) {
 
     // start Fastify server
     const address = await server.listen({ port, host: "0.0.0.0" });
-    logger.info(`Server listening at ${address}`)
+    logger.info(`Server listening at ${address}`);
+
+    // graceful shutdown - https://thatarif.in/posts/token-based-authentication-with-fastify-jwt
+    const listeners = ['SIGINT', 'SIGTERM'];
+    listeners.forEach((signal) => {
+      process.on(signal, async () => {
+        await server.close();
+        process.exit(0);
+      });
+    });
   } catch (err) {
     //logger.error(err);
     logger.error(err);
