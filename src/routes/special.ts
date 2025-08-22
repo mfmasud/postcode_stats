fcbb5403b0026dce7cb95dbc738c672248d8698e
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-08-22 16:32:08 +0000

    split special routes into special and auth

--- src/routes/special.ts
+++ src/routes/special.ts
@@ -7,14 +7,47 @@ import logger from '../../utils/logger.js';
  */
 async function specialRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
 
-    fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
-        logger.info(
-          `Incoming ${request.method} request from ${request.socket.remoteAddress} to ${request.headers.host}${request.url}`
-        );
-        return {
-          message: "Hello and welcome to the UK Location API!",
-        };
+    fastify.get('/', async (request) => {
+      return Welcome(request);
     });
+
+    fastify.get('/user', UserDetails);
+}
+
+// This function is independent of Fastify's route registration
+export function Welcome(request: FastifyRequest) {
+  logger.info(
+    `Incoming ${request.method} request from ${request.ip} to ${request.headers.host}${request.url}`
+  );
+
+  return {
+    message: 'Hello and welcome to the UK Location API!',
+  };
+}
+
+/**
+ * A function which displays the details of the current user. If no user has been authenticated, a message is returned saying so.
+ *
+ * @function UserDetails
+ *
+ * @param {Object} ctx - The context for the function call, representing the request made to it.
+ * @returns {undefined} ctx is modified with a 200 status code and a body representing all the user's details.
+ */
+function UserDetails(ctx) {
+  logger.info(`UserDetails accessed from ${ctx.request.ip}`);
+  const user = ctx.state.user;
+
+  if (!user) {
+    logger.info(`User not logged in`);
+    ctx.throw(401, "No user details available. Please log in to see them.");
+    return;
+  }
+
+  logger.info(`Details returned for User "${user.username}" in message body.`);
+  ctx.status = 200;
+  ctx.body = {
+    user,
+  };
 }
 
 export default specialRoutes;
