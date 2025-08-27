ae4046dbc72e5769519a52fc30d1988d513b5b97
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-08-27 13:09:26 +0000

    update jwt script

--- docs/notes/set_jwt.js
+++ docs/notes/set_jwt.js
@@ -4,46 +4,39 @@ Small script to set JWT tokens for the 3 users using postman.
 These can then be used in the authorisation header for requests by setting
 the auth tab to Bearer token and entering the token like this: "{{JWT_standard}}"
 
-These are set as collection variables so they can be used in any request in
+These are set as Postman environment variables so they can be used in any request in
 the collection e.g. For testing searches or modifying users.
 
-Requires {{base_url}} to be set e.g. pm.setEnvironmentVariable("base_url", "http://localhost:3000/api/v1");
+Requires {{base_url}} to be set e.g. pm.environment.set("base_url", "http://localhost:3000/api/v1");
 
 */
 
-// id = 1 - standard
-pm.variables.unset("jwt_token");
-var url = (pm.environment.get("base_url") + "/login/?id=1");
 
-pm.sendRequest(url, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-        var jwt = response.json().jwt;
-        pm.collectionVariables.set("JWT_standard", jwt)
-  }
-});
+function setJWT(user) {
+  const { id, name } = user;
+  var url = `${pm.environment.get("base_url")}/login/?id=${id}`;
 
-var url = (pm.environment.get("base_url") + "/login/?id=2");
+  pm.sendRequest(url, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      var jwt = response.json().jwt;
+      pm.environment.set(`JWT_${name}`, jwt);
 
-// id = 2 - paid
-pm.sendRequest(url, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-        var jwt = response.json().jwt;
-        pm.collectionVariables.set("JWT_paid", jwt)
-  }
-});
+      // Set jwt_token in environment
+      if (id === 3) {
+        pm.environment.set("jwt_token", jwt);
+      }
+    }
+  });
+}
 
-var url = (pm.environment.get("base_url") + "/login/?id=3");
+const users = [
+  { id: 1, name: "standard" },
+  { id: 2, name: "paid" },
+  { id: 3, name: "admin" }
+];
 
-// id = 3 - admin
-pm.sendRequest(url, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-        var jwt = response.json().jwt;
-        pm.collectionVariables.set("JWT_admin", jwt)
-  }
-});
+for (const user of users) {
+  setJWT(user);
+}
