f0a384a99ae7b2839bff40ab6b18e4b68cf3da07
Mohammed Fardhin Masud <fardhinmasud@gmail.com>
2025-10-04 15:11:14 +0000

    move search functionality to its own plugin

--- src/plugins/search/searchHelper.js
+++ src/plugins/search/searchHelper.js
@@ -18,13 +18,13 @@
  *
  */
 
-const Atco = require("../models/Atco");
-const CrimeList = require("../models/CrimeList");
+const Atco = require("../../models/Atco")
+const CrimeList = require("../../models/CrimeList")
 
-const logger = require("../utils/logger");
+const logger = require("../../utils/logger")
 
-const { queryAtcoAPI } = require("../helpers/AtcoCodes");
-const { getCrimeData } = require("../helpers/crime");
+const { queryAtcoAPI } = require("../../helpers/AtcoCodes")
+const { getCrimeData } = require("../../helpers/crime")
 
 /**
  * Links a `CrimeList` to the `Search` model by comparing the latitude value used by both.
@@ -42,21 +42,21 @@ const { getCrimeData } = require("../helpers/crime");
  * @see {@link getRelatedCrimes} - Adds the `Crime`s to the `Search` model.
  */
 async function linkCrimeList(SearchModel) {
-  // links crime list to search model
-  // can be linked by latitude - assuming it is available. If not, this should have been calculated beforehand.
+    // links crime list to search model
+    // can be linked by latitude - assuming it is available. If not, this should have been calculated beforehand.
 
-  const linkedCrimes = await CrimeList.findOne({
-    latitude: SearchModel.latitude,
-  });
-  if (linkedCrimes) {
-    SearchModel.linkedCrimeList = linkedCrimes;
-    logger.info("Successfully linked crime list to search");
-  } else {
-    logger.error("Could not link crime list to search.");
-    return;
-  }
+    const linkedCrimes = await CrimeList.findOne({
+        latitude: SearchModel.latitude,
+    })
+    if (linkedCrimes) {
+        SearchModel.linkedCrimeList = linkedCrimes
+        logger.info("Successfully linked crime list to search")
+    } else {
+        logger.error("Could not link crime list to search.")
+        return
+    }
 
-  await SearchModel.save();
+    await SearchModel.save()
 }
 
 /**
@@ -73,30 +73,30 @@ async function linkCrimeList(SearchModel) {
  * @see {@link linkCrimeList} - links the `CrimeList` to the `Search` model for use in this function.
  */
 async function getRelatedCrimes(SearchModel) {
-  const { latitude, longitude } = SearchModel;
+    const { latitude, longitude } = SearchModel
 
-  if (SearchModel.Postcode.country === "Northern Ireland") {
-    // NI not handled by policing API.
-    logger.info("Cannot link NI CrimeList.");
-    return;
-  }
-
-  if (latitude && longitude) {
-    await getCrimeData(latitude, longitude); // fetches the Police API
-    await linkCrimeList(SearchModel); // Links the CrimeList data to the Search model
-    if (SearchModel.linkedCrimeList) {
-      logger.info("Added crimes to search");
-      SearchModel.queryCrimes = SearchModel.linkedCrimeList.crimes;
-    } else {
-      SearchModel.queryCrimes = []; // empty to indicate not found
+    if (SearchModel.Postcode.country === "Northern Ireland") {
+        // NI not handled by policing API.
+        logger.info("Cannot link NI CrimeList.")
+        return
     }
-  } else {
-    logger.error("Need LAT/LONG Coordinates to use the Police API.");
-    SearchModel.queryCrimes = []; // empty to indicate not found
-  }
 
-  await SearchModel.save();
-  return;
+    if (latitude && longitude) {
+        await getCrimeData(latitude, longitude) // fetches the Police API
+        await linkCrimeList(SearchModel) // Links the CrimeList data to the Search model
+        if (SearchModel.linkedCrimeList) {
+            logger.info("Added crimes to search")
+            SearchModel.queryCrimes = SearchModel.linkedCrimeList.crimes
+        } else {
+            SearchModel.queryCrimes = [] // empty to indicate not found
+        }
+    } else {
+        logger.error("Need LAT/LONG Coordinates to use the Police API.")
+        SearchModel.queryCrimes = [] // empty to indicate not found
+    }
+
+    await SearchModel.save()
+    return
 }
 
 /**
@@ -114,9 +114,9 @@ async function getRelatedCrimes(SearchModel) {
  *
  */
 async function getRelatedListings(SearchModel) {
-  const { latitude, longitude } = SearchModel;
-  //await getPropertyData(latitude, longitude);
-  return;
+    const { latitude, longitude } = SearchModel
+    //await getPropertyData(latitude, longitude);
+    return
 }
 
 /**
@@ -139,37 +139,37 @@ async function getRelatedListings(SearchModel) {
  * @see {@link getWalesLocations} - Generates the `Atco` model's `other_names` field for Wales.
  */
 async function searchAtco(PostcodeModel) {
-  var {
-    admin_county,
-    admin_district,
-    parliamentary_constituency,
-    region,
-    country,
-  } = PostcodeModel;
-  //logger.info(admin_county, admin_district, region);
+    var {
+        admin_county,
+        admin_district,
+        parliamentary_constituency,
+        region,
+        country,
+    } = PostcodeModel
+    //logger.info(admin_county, admin_district, region);
 
-  if (country === "Scotland") {
-    region = "Scotland";
-  }
+    if (country === "Scotland") {
+        region = "Scotland"
+    }
 
-  const pc = parliamentary_constituency;
+    const pc = parliamentary_constituency
 
-  // starting to look ugly - maybe use functions or switch/case
-  if (!admin_county) {
-    logger.info("No admin county");
+    // starting to look ugly - maybe use functions or switch/case
+    if (!admin_county) {
+        logger.info("No admin county")
 
-    if (!admin_district) {
-      // no admin district
-      logger.info("No admin district");
-    } else {
-      // admin district exists
-      var AtcoToLink = await Atco.findOne({ location: admin_district });
-      if (!AtcoToLink) {
-        AtcoToLink = await Atco.findOne({ other_names: admin_district });
-      }
+        if (!admin_district) {
+            // no admin district
+            logger.info("No admin district")
+        } else {
+            // admin district exists
+            var AtcoToLink = await Atco.findOne({ location: admin_district })
+            if (!AtcoToLink) {
+                AtcoToLink = await Atco.findOne({ other_names: admin_district })
+            }
 
-      if (region === "London") {
-        /*
+            if (region === "London") {
+                /*
         example: Lambeth - Local Authority / London Borough
 
         Postcode API data:
@@ -181,37 +181,39 @@ async function searchAtco(PostcodeModel) {
 
         This also includes postcodes in the City of London (e.g. E1 7DA)
         */
-        var AtcoToLink = await Atco.findOne({ location: "Greater London" });
-      }
+                var AtcoToLink = await Atco.findOne({
+                    location: "Greater London",
+                })
+            }
 
-      if (!AtcoToLink && pc) {
-        // parlimentiary constituency, e.g. Poole (South West)
-        // Others like Bournemout East would be under Dorset (ceremonial country)
-        var AtcoToLink = await Atco.findOne({ location: pc });
-        if (!AtcoToLink) {
-          AtcoToLink = await Atco.findOne({ other_names: pc });
+            if (!AtcoToLink && pc) {
+                // parlimentiary constituency, e.g. Poole (South West)
+                // Others like Bournemout East would be under Dorset (ceremonial country)
+                var AtcoToLink = await Atco.findOne({ location: pc })
+                if (!AtcoToLink) {
+                    AtcoToLink = await Atco.findOne({ other_names: pc })
+                }
+            }
         }
-      }
+    } else {
+        // admin county exists
+        var AtcoToLink = await Atco.findOne({ location: admin_county })
+        if (!AtcoToLink) {
+            AtcoToLink = await Atco.findOne({ other_names: admin_county })
+        }
     }
-  } else {
-    // admin county exists
-    var AtcoToLink = await Atco.findOne({ location: admin_county });
-    if (!AtcoToLink) {
-      AtcoToLink = await Atco.findOne({ other_names: admin_county });
-    }
-  }
 
-  if (AtcoToLink) {
-    // match found
-    logger.info(
-      `Found matching ATCO: code ${AtcoToLink.code} matches ${AtcoToLink.location}, ${AtcoToLink.region}`
-    );
-    await queryAtcoAPI(AtcoToLink.code);
-    return AtcoToLink;
-  } else {
-    logger.info("Matching ATCO not found");
-    return;
-  }
+    if (AtcoToLink) {
+        // match found
+        logger.info(
+            `Found matching ATCO: code ${AtcoToLink.code} matches ${AtcoToLink.location}, ${AtcoToLink.region}`
+        )
+        await queryAtcoAPI(AtcoToLink.code)
+        return AtcoToLink
+    } else {
+        logger.info("Matching ATCO not found")
+        return
+    }
 }
 
 /**
@@ -229,28 +231,28 @@ async function searchAtco(PostcodeModel) {
  *
  */
 async function linkAtco(SearchModel) {
-  // links Search model to correct ATCO from available information.
-  // Should be run when search is created / postcode is updated.
-  // Does not return anything
-  //logger.info("Looking to link Atco");
+    // links Search model to correct ATCO from available information.
+    // Should be run when search is created / postcode is updated.
+    // Does not return anything
+    //logger.info("Looking to link Atco");
 
-  var linkedAtco;
+    var linkedAtco
 
-  if (SearchModel.Postcode.country === "Northern Ireland") {
-    // Skip ATCO linking for northern irish postcodes e.g. BT23 6SA
-    // linkedAtco = linkOther(SearchModel.Postcode)
-    logger.info("Cannot link NI Atco.");
-    return;
-  } else {
-    linkedAtco = await searchAtco(SearchModel.Postcode);
-  }
+    if (SearchModel.Postcode.country === "Northern Ireland") {
+        // Skip ATCO linking for northern irish postcodes e.g. BT23 6SA
+        // linkedAtco = linkOther(SearchModel.Postcode)
+        logger.info("Cannot link NI Atco.")
+        return
+    } else {
+        linkedAtco = await searchAtco(SearchModel.Postcode)
+    }
 
-  if (!linkedAtco) {
-    return;
-  }
+    if (!linkedAtco) {
+        return
+    }
 
-  SearchModel.linkedATCO = linkedAtco;
-  await SearchModel.save();
+    SearchModel.linkedATCO = linkedAtco
+    await SearchModel.save()
 }
 
 /**
@@ -269,22 +271,22 @@ async function linkAtco(SearchModel) {
  * @see {@link linkAtco} - Links the `Atco` model to the `Search` model.
  */
 async function getRelatedStops(SearchModel, radius = 1000) {
-  // bus stops around a 1km radius from a given point. maximum returned should be 4 points (arbitrary numbers)
-  const { longitude, latitude, Northing, Easting } = SearchModel;
+    // bus stops around a 1km radius from a given point. maximum returned should be 4 points (arbitrary numbers)
+    const { longitude, latitude, Northing, Easting } = SearchModel
 
-  let linkedAtco = await SearchModel.populate("linkedATCO");
-  linkedAtco = linkedAtco.linkedATCO;
+    let linkedAtco = await SearchModel.populate("linkedATCO")
+    linkedAtco = linkedAtco.linkedATCO
 
-  // for java : https://stackoverflow.com/questions/22063842/check-if-a-latitude-and-longitude-is-within-a-circle
-  // first check if there are latitude/ longitude for the query bus stop then search within the radius from SearchModel.latitude / longitude
-  // just returning first 5 for now...
-  // Need to convert BNG to lat/long for empty values or search using BNG instead. See https://github.com/chrisveness/geodesy/blob/master/osgridref.js to convert
+    // for java : https://stackoverflow.com/questions/22063842/check-if-a-latitude-and-longitude-is-within-a-circle
+    // first check if there are latitude/ longitude for the query bus stop then search within the radius from SearchModel.latitude / longitude
+    // just returning first 5 for now...
+    // Need to convert BNG to lat/long for empty values or search using BNG instead. See https://github.com/chrisveness/geodesy/blob/master/osgridref.js to convert
 
-  if (linkedAtco) {
-    SearchModel.queryBusStops = linkedAtco.busstops.slice(0, 5);
-  }
+    if (linkedAtco) {
+        SearchModel.queryBusStops = linkedAtco.busstops.slice(0, 5)
+    }
 
-  await SearchModel.save();
+    await SearchModel.save()
 }
 
 /**
@@ -301,35 +303,35 @@ async function getRelatedStops(SearchModel, radius = 1000) {
  * @see {@link https://en.wikipedia.org/wiki/HATEOAS} - HATEOAS compliant API responses.
  */
 async function updateLinks(context, SearchModel) {
-  let lat = SearchModel.latitude;
-  let long = SearchModel.longitude;
-  const postcode = SearchModel.Postcode.postcode;
-  const hostname = context.req.headers.host;
+    let lat = SearchModel.latitude
+    let long = SearchModel.longitude
+    const postcode = SearchModel.Postcode.postcode
+    const hostname = context.req.headers.host
 
-  SearchModel._links = {};
+    SearchModel._links = {}
 
-  // SearchModel._links.alternate = `${hostname}/api/v1/search/${searchModel.searchID}`;
-  SearchModel._links.postcode = {
-    href: `https://${hostname}/api/v1/postcodes/${postcode}`,
-  };
+    // SearchModel._links.alternate = `${hostname}/api/v1/search/${searchModel.searchID}`;
+    SearchModel._links.postcode = {
+        href: `https://${hostname}/api/v1/postcodes/${postcode}`,
+    }
 
-  if (lat && long) {
-    SearchModel._links.self = {
-      href: `https://${hostname}/api/v1/search/?latitude=${lat}&longitude=${long}`,
-    };
-  }
+    if (lat && long) {
+        SearchModel._links.self = {
+            href: `https://${hostname}/api/v1/search/?latitude=${lat}&longitude=${long}`,
+        }
+    }
 
-  try {
-    //logger.info(JSON.stringify(SearchModel._links));
-    await SearchModel.save();
-  } catch (err) {
-    logger.error(err);
-  }
+    try {
+        //logger.info(JSON.stringify(SearchModel._links));
+        await SearchModel.save()
+    } catch (err) {
+        logger.error(err)
+    }
 }
 
 module.exports = {
-  getRelatedStops,
-  getRelatedCrimes,
-  linkAtco,
-  updateLinks,
-};
+    getRelatedStops,
+    getRelatedCrimes,
+    linkAtco,
+    updateLinks,
+}
